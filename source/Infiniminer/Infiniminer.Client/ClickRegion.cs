a66058cdcdf795f5ee26d63fef7bb193ad1338fd
Christopher Whitley <aristurtledev@gmail.com>
2023-10-08 13:51:36 -0400

    Port from XNA to MonoGame

--- source/Infiniminer/Infiniminer.Client/ClickRegion.cs
+++ source/Infiniminer/Infiniminer.Client/ClickRegion.cs
@@ -7,12 +7,9 @@ using StateMasher;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
-using Microsoft.Xna.Framework.Net;
-using Microsoft.Xna.Framework.Storage;
 
 namespace Infiniminer
 {
